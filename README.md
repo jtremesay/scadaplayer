0acc2e3584c568b4a5e5e33b53553a147e1d0628
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2023-12-21 19:54:45 +0100

    move code source

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# scadaplayer
+# Scadaplayer
 
-A thing for visualizing SCADA files.
+Demo available here: https://jtremesay.org/pages/scadaplayer.html
+Code source moved here: https://github.com/jtremesay/jtremesay.org/blob/4b43417b73fd8a0577e4cd2dd2b4349647cd5400/front/main/scadaplayer.ts
--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { Engine } from "./engine"
-import { parse_scada } from "./parser"
-
-const metadata = JSON.parse((document.getElementById("metadata")! as HTMLTextAreaElement).value)
-const records = parse_scada((document.getElementById("scada")! as HTMLTextAreaElement).value)
-const ui = new Engine(metadata, records)
-
-ui.request_frame()
